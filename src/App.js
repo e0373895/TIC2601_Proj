a263bd1fc4bdd04affa6abd949e5824bc79ed702
BearTeddy <bearteddy.dvplr@gmail.com>
2019-11-01 20:51:39 +0800

    Commit FIles

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/logo/logo";
+import "./App.css";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: "signin",
+      isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: ""
+      }
+    };
+  }
+  loadUser = data => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    });
+  };
+
+  onRouteChange = route => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
+  render() {
+    const { isSignedIn, route } = this.state;
+    return (
+      <div className="App">
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {route === "home" ? (
+          <div>
+            <Logo />
+          </div>
+        ) : route === "signin" ? (
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
